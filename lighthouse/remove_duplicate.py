713544a555711f69b4fcb874ea7d64448d101260
Julian Lee <julianlee@linkerviison.com>
2025-03-07 17:19:23 +0800

    Script to remove duplicate from deduplicate image list

--- lighthouse/remove_duplicate.py
+++ lighthouse/remove_duplicate.py
@@ -2,54 +2,101 @@ import json
 import pathlib
 
 # load new anno.json , save to revised anno
-new_anno_list = [
-    # only this two use this path to do vlm annotations
-    '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95_rededuplicate.json',
-    '/mnt/data-home/mobility-multimodal/data-curation/Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95_rededuplicate.json',
-    # 
-    # '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95_rededuplicate.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250115/Transportation_20250115_image_list_keep_0.95_rededuplicate.json',
-]
+#todo_anno_list = [
+#    # only this two use this path to do vlm annotations
+#    '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95_rededuplicate.json',
+#    '/mnt/data-home/mobility-multimodal/data-curation/Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95_rededuplicate.json',
+#    # 
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95_rededuplicate.json',
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250115/Transportation_20250115_image_list_keep_0.95_rededuplicate.json',
+#]
+#
+## TODO deal with this tomorrow after gpt done load vlm_annoataions.json, save to revised anno
+#vlm_anno_list = [
+#    # '/mnt/data-home/mobility-multimodal/data-curation/Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95_rededuplicate.json',
+#
+#    '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95_rededuplicate.json',
+#]
+#
+#todo_anno_list = vlm_anno_list
 
-# TODO deal with this tomorrow after gpt done load vlm_annoataions.json, save to revised anno
-vlm_anno_list = [
-    '/mnt/data-home/mobility-multimodal/data-curation/Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95_rededuplicate.json',
-]
 
 
-for image_path_file in new_anno_list:
-    with open(image_path_file, 'r') as f:
-        image_path = json.load(f)
-    image_set = {pathlib.Path(image).stem for image in image_path}
-    data_path = image_path_file.replace('data-curation', 'vlm-annotations').replace('image_list_keep_0.95_rededuplicate.json', 'image_list_keep_0.95')
-    data_path = pathlib.Path(data_path)
-    anno_path = data_path / 'annotations'
-    anno_jsons = list(anno_path.rglob('new_anno_0306.json'))
-    annos = []
+
+# for image_path_file in todo_anno_list:
+#     with open(image_path_file, 'r') as f:
+#         image_path = json.load(f)
+#     image_set = {pathlib.Path(image).stem for image in image_path}
+#     dataverse_path = image_path_file.replace('data-curation', 'vlm-annotations').replace('image_list_keep_0.95_rededuplicate.json', 'image_list_keep_0.95')
+#     dataverse_path = pathlib.Path(dataverse_path)
+#     anno_path = dataverse_path / 'annotations'
+#     anno_jsons = list(anno_path.rglob('new_anno_0306.json'))
+#     annos = []
+#     for anno_json in anno_jsons:
+#         with open(anno_json, 'r') as f:
+#             data = json.load(f)
+#         annos.extend(data)
+
+#     revised_annos = []
+#     image_root = dataverse_path / 'images'
+#     for anno in annos:
+#         image_stem = anno['image'].split('.')[0]
+#         if image_stem in image_set:
+#             revised_annos.append(anno)
+#             continue
+#         print(anno['image'])
+#         if (image_root / anno['image']).exists():
+#             print(f'removing {anno["image"]}')
+#             (image_root / anno['image']).unlink()
+
+#     revised_anno_path = dataverse_path / 'annotations' / f'revised_anno.json'
+#     with open(revised_anno_path, 'w') as f:
+#         json.dump(revised_annos, f, ensure_ascii=False, indent=4)
+
+
+ 
+def remove_duplicated(dataverse_path, rededuplicated_json, old_anno_str='*new_anno_0306.json', new_anno_name='revised_anno.json'):
+    anno_path = dataverse_path / 'annotations'
+    anno_jsons = list(anno_path.rglob(old_anno_str))
+    anno_data = []
     for anno_json in anno_jsons:
         with open(anno_json, 'r') as f:
             data = json.load(f)
-        annos.extend(data)
+        anno_data.extend(data)
 
     revised_annos = []
-    image_root = data_path / 'images'
-    for anno in annos:
-        image_stem = anno['image'].split('.')[0]
+    image_root = dataverse_path / 'images'
+
+    with open(rededuplicated_json, 'r') as f:
+        image_path = json.load(f)
+    image_set = {pathlib.Path(image).stem for image in image_path}
+    print(image_set)
+
+    removed_count = 0
+    for anno in anno_data:
+        image_stem = ('.').join(anno['image'].split('.')[:-1])
+        # import ipdb; ipdb.set_trace()
         if image_stem in image_set:
             revised_annos.append(anno)
             continue
-        print(anno['image'])
+        # print(anno['image'])
+        removed_count += 1
         if (image_root / anno['image']).exists():
             print(f'removing {anno["image"]}')
             (image_root / anno['image']).unlink()
 
-    revised_anno_path = data_path / 'annotations' / f'revised_anno.json'
+    revised_anno_path = dataverse_path / 'annotations' / new_anno_name
     with open(revised_anno_path, 'w') as f:
         json.dump(revised_annos, f, ensure_ascii=False, indent=4)
 
+    print(f'removed {removed_count} images')
 
- 
+
+if __name__ == '__main__':
+    dataverse_path = '/mnt/data-home/mobility-multimodal/vlm-annotations/Sports_Development/20250109/Sports_Development_20250109_llava-onevision-0.5b-full/'
+    rededuplicated_json = '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95_rededuplicate.json'
+    remove_duplicated(pathlib.Path(dataverse_path), rededuplicated_json)
