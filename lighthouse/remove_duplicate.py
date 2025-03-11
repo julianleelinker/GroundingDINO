950155a743b7a6fb134040d84f45e3747d6f7eac
Julian Lee <julianlee@linkerviison.com>
2025-03-11 18:43:43 +0800

    Some shit

--- lighthouse/remove_duplicate.py
+++ lighthouse/remove_duplicate.py
@@ -3,9 +3,21 @@ import pathlib
 import tqdm
 
 
-def remove_duplicated(dataverse_path, rededuplicated_json, old_anno_str='*new_anno_0306.json', new_anno_name='revised_anno.json', dry_run=True, verbose=True):
+DEPART_LIST = [
+    'China_Steel',
+    'Mass_Rapid_Transit',
+    'Ports_Corporation',
+    'Public_Works',
+    'Sports_Development',
+    'Taiwan_Power',
+    'Transportation',
+    'Water_Resources',
+]
+
+
+def remove_duplicated(dataverse_path, rededuplicated_json, old_anno_name='new_anno_0306.json', new_anno_name='anno_0311.json', dry_run=True, verbose=True):
     anno_path = dataverse_path / 'annotations'
-    anno_jsons = list(anno_path.rglob(old_anno_str))
+    anno_jsons = list(anno_path.rglob(old_anno_name))
     anno_data = []
     for anno_json in anno_jsons:
         with open(anno_json, 'r') as f:
@@ -17,15 +29,30 @@ def remove_duplicated(dataverse_path, rededuplicated_json, old_anno_str='*new_an
 
     with open(rededuplicated_json, 'r') as f:
         image_path = json.load(f)
-    image_set = {pathlib.Path(image).stem for image in image_path}
-    print(len(image_set))
-    # import ipdb; ipdb.set_trace()
 
-    removed_count = 0
+    # temp
+    rede_image_set = set()
+    for image in image_path:
+        src_file = pathlib.Path(image).resolve()
+        for depart in DEPART_LIST:
+            if depart in str(src_file):
+                break
+        folder_list = str(src_file).split(f'/{depart}/')
+        dst_name = ('-').join(folder_list[1:]).replace('/', '-')
+        dst_name = ('.').join(dst_name.split('.')[:-1]) + '.' + dst_name.split('.')[-1].lower()
+        rede_image_set.add(dst_name)
+    
+
+    # rede_image_set = {pathlib.Path(image).stem for image in image_path}
+    print(f'{len(rede_image_set)=}')
+
+    removed_count, new_count = 0, 0
     for anno in tqdm.tqdm(anno_data):
-        image_stem = ('.').join(anno['image'].split('.')[:-1])
-        if image_stem in image_set:
+        # image_stem = ('.').join(anno['image'].split('.')[:-1])
+        
+        if anno['image'] in rede_image_set:
             revised_annos.append(anno)
+            new_count += 1
             continue
 
         removed_count += 1
@@ -36,11 +63,13 @@ def remove_duplicated(dataverse_path, rededuplicated_json, old_anno_str='*new_an
 
     if dry_run:
         print(f'dry run, removed {removed_count} images from {dataverse_path}')
+        print(f'dry run, new count {new_count} images from {dataverse_path}')
     else:
         revised_anno_path = dataverse_path / 'annotations' / new_anno_name
         with open(revised_anno_path, 'w') as f:
             json.dump(revised_annos, f, ensure_ascii=False, indent=4)
         print(f'removed {removed_count} images from {dataverse_path}')
+        print(f'new count {new_count} images from {dataverse_path}')
 
 
 def check_json(old_json, rede_json):
@@ -79,40 +108,23 @@ if __name__ == '__main__':
     vlm_prefix = '/mnt/data-home/mobility-multimodal/vlm-annotations'
 
     path_map = {
-        "Sports_Development/20241223/Sports_Development_20241223_curated_t7-revised":                 'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-        # "Sports_Development/20241223/Sports_Development_20241223_curated_t5_VLA_makeup-revised":      'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-        "Sports_Development/20241223/Sports_Development_20241223_curated_t5_VLA_patch-revised":      'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-        "Transportation/20250109/Transportation_20250109_curated_t7-revised":                         'Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
-        "Transportation/20241230/Transportation_20241230_curated_t7-revised":                         'Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
-        "Public_Works/20241230/Public_Works_20241230_curated_t5_part-revised":                        'Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
-
-        # "Sports_Development/20250109/Sports_Development_20250109_llava-onevision-0.5b-full-revised":  'Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95_rededuplicate.json',
-        # "Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95-revised":                   'Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95_rededuplicate.json',
-        # "Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95-revised":                   'Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95_rededuplicate.json',  
-        # "Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95-revised":       'Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95_rededuplicate.json',
-        
-
-
-        # "Water_Resources/20250106/Water_Resources_20250106_curated_t5.json-revised":                       'Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95_rededuplicate.json',
+        # "Sports_Development/20241223/Sports_Development_20241223_curated_t7-revised":           'Sports_Development/20241223/Sports_Development_20241223_curated_new.json',
+        # "Sports_Development/20241223/Sports_Development_20241223_curated_t5_VLA_patch-revised": 'Sports_Development/20241223/Sports_Development_20241223_curated_new.json',
+        # "Transportation/20250109/Transportation_20250109_curated_t7-revised":                   'Transportation/20250109/Transportation_20250109_curated_t7_new.json',
+        # "Transportation/20241230/Transportation_20241230_curated_t7-revised":                   'Transportation/20241230/Transportation_20241230_curated_t7_new.json',           
+        # "Public_Works/20241230/Public_Works_20241230_curated_t5_part-revised":                  'Public_Works/20241230/Public_Works_20241230_curated_t5_part_new.json',
+        "Water_Resources/20250106/Water_Resources_20250106_curated_t5-revised":                   'Water_Resources/20250106/Water_Resources_20250106_curated_t5_new.json'
     }
-    # for anno_path, rededu_path in path_map.items():
-    #     remove_duplicated(pathlib.Path(f'{vlm_prefix}/{anno_path}'), f'{curation_prefix}/{rededu_path}', verbose=False, dry_run=True)
+    for anno_path, rededu_path in path_map.items():
+        remove_duplicated(pathlib.Path(f'{vlm_prefix}/{anno_path}'), f'{curation_prefix}/{rededu_path}', new_anno_name='new_anno_0306.json', verbose=False, dry_run=False)
 
-    json_map = {key.replace('-revised', '.json'): value for key, value in path_map.items()}
-    for json_old, json_rede in json_map.items():
-        check_json(pathlib.Path(f'{curation_prefix}/{json_old}'), f'{curation_prefix}/{json_rede}')
-
-
-"Sports_Development/20241223/Sports_Development_20241223_curated_t7.json":                 'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-"Sports_Development/20241223/Sports_Development_20241223_curated_t5_VLA_patch.json":       'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-"Transportation/20250109/Transportation_20250109_curated_t7.json":                         'Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
-"Transportation/20241230/Transportation_20241230_curated_t7.json":                         'Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
-"Public_Works/20241230/Public_Works_20241230_curated_t5_part.json":                        'Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
-
-   
- 
-   
-   
-   
+    # json_map = {key.replace('-revised', '.json'): value for key, value in path_map.items()}
+    # for json_old, json_rede in json_map.items():
+    #     check_json(pathlib.Path(f'{curation_prefix}/{json_old}'), f'{curation_prefix}/{json_rede}')
 
 
+#"Sports_Development/20241223/Sports_Development_20241223_curated_t7.json":                 'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
+#"Sports_Development/20241223/Sports_Development_20241223_curated_t5_VLA_patch.json":       'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
+#"Transportation/20250109/Transportation_20250109_curated_t7.json":                         'Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
+#"Transportation/20241230/Transportation_20241230_curated_t7.json":                         'Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
+#"Public_Works/20241230/Public_Works_20241230_curated_t5_part.json":                        'Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
--- tools/copy_remote_images_in_json.py
+++ tools/copy_remote_images_in_json.py
@@ -17,27 +17,41 @@ SOURCES_BY_DEPART = {
     'Linker_Vision_Data_V2': 0,
     'Kaohsiung_Data_V2': 0,
 }
+prefix = '/mnt/data-home/mobility-multimodal/data-curation'
 JSON_LIST_JAN = [
-    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20241230/Transportation_20241230_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250109/Transportation_20250109_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95.json',
-    '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95.json',
+    # 'Transportation/20241230/Transportation_20241230_image_list_keep_0.95.json',
+    # 'Transportation/20250109/Transportation_20250109_image_list_keep_0.95.json',
+    'Transportation/20250115/Transportation_20250115_image_list_keep_0.95_rededuplicate.json',
+    'Transportation/20250120/Transportation_20250120_image_list_keep_0.95.json',
+
+    # 'Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95.json',
+    'Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95.json',
+    'Public_Works/20250206/Public_Works_20250206_image_list_keep_0.95.json',
+
+    # 'Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95.json',
+    'Water_Resources/20250213/Water_Resources_20250213_image_list_keep_0.95.json',
+
+    # 'Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95.json',
+
+    # 'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95.json',
+    # 'Sports_Development/20250109/Sports_Development_20250109_image_list_keep_0.95.json',
+
+    # 'Taiwan_Power/20250106/Taiwan_Power_20250106_image_list_keep_0.95.json',
+
+    'Ports_Corporation/20250124/Ports_Corporation_20250124_image_list_keep_0.95.json'
+
+
     # below include too much images
-    # '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Linker_Vision_Data_V2/linker_4M_image_list_keep_0.95.json',
-    # '/mnt/data-home/mobility-multimodal/data-curation/Kaohsiung_Data_V2/Kaohsiung_Data_V2_image_list_keep_0.95.json',
+    # 'Linker_Vision_Data_V2/linker_4M_image_list_keep_0.95.json',
 ]
 
-JSON_LIST_JAN = [
+# JSON_LIST_JAN = [
     # '/mnt/data-home/mobility-multimodal/data-curation/Public_Works/20250106/Public_Works_20250106_image_list_keep_0.95.json',
     # '/mnt/data-home/mobility-multimodal/data-curation/Water_Resources/20250106/Water_Resources_20250106_image_list_keep_0.95.json',
     # '/mnt/data-home/mobility-multimodal/data-curation/Transportation/20250109/Transportation_20250109_image_list_keep_0.95.json',
-    '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95.json',
-]
+    # '/mnt/data-home/mobility-multimodal/data-curation/Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95.json',
+# ]
+JSON_LIST_JAN = [f'{prefix}/{json_path}' for json_path in JSON_LIST_JAN]
 
 ROOT_LIST = {
     'Dec',
@@ -50,8 +64,6 @@ ROOT_LIST = {
     'Jul',
     'Aug',
 }
-
-REMOTE_HOST = 'julian@10.1.24.90'
 
 
 def get_concat_file_name(file_path, root_list):
@@ -71,56 +83,4 @@ if __name__ == '__main__':
         with open(json_path, 'r') as f:
             image_list = json.load(f)
         print(f'{json_path=}')
-        print(f'{len(image_list)=}')
-        for key, val in SOURCES_BY_DEPART.items():
-            if key in json_path:
-                SOURCES_BY_DEPART[key] += len(image_list)
-                # count += len(image_list)
-                break
-    print('images numbers from different sources:')
-    for key, val in SOURCES_BY_DEPART.items():
-        print(f'{key}, {val}')
-    print('continue to copy?')
-
-    
-    for json_path in JSON_LIST_JAN:
-        print(json_path)
-        with open(json_path, 'r') as f:
-            json_data = json.load(f)
-
-        image_path_list = [pathlib.Path(data) for data in json_data]
-        # if len(image_path_list) > sample_size:
-        #     image_path_list = random.sample(image_path_list, sample_size)
-        output_folder = pathlib.Path(output_root) / pathlib.Path(json_path).stem
-        output_folder.mkdir(parents=True, exist_ok=True)
-
-        files_per_folder = 5000
-        for i in range(1, (len(image_path_list)-1)//files_per_folder + 2):
-            (output_folder / f'split{i}').mkdir(parents=True, exist_ok=True)
-            # os.makedirs(os.path.join(dest_dir, f"split_{i}"), exist_ok=True)
-    
-        # for index, filename in enumerate(image_path_list):
-        #     # Determine the folder index (round-robin)
-
-        #     src_path = os.path.join(source_dir, filename)
-        #     dest_path = os.path.join(dest_dir, f"split_{folder_index}", filename)
-
-
-        duplicated_count = 0
-        for index, remote_path in tqdm.tqdm(enumerate(image_path_list), total=len(image_path_list)):
-            image_name = get_concat_file_name(remote_path, ROOT_LIST)
-            folder_index = (index // files_per_folder) + 1
-            local_path = output_folder / f'split{folder_index}' / image_name
-            if not local_path.exists():
-                command = f"rsync -avz {REMOTE_HOST}:{remote_path} {local_path}"
-                subprocess.run(["rsync", "-avz", f"{REMOTE_HOST}:{remote_path}", local_path], check=False)
-            else:
-                duplicated_count += 1
-                print(f'{remote_path} already exist, skip')
-        print(f'{duplicated_count=}')
-        print(f'file saved to {str(output_folder)}')
-
-
-# Water_Resources_20250106_image_list_keep_0.95 many
-# Transportation_20250109_image_list_keep_0.95 
-# Sports_Development_20241223_image_list_keep_0.95
+        print(f'{len(image_list)=}\n')
