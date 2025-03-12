f1a124c32333f65b3cb3b4f4a8f119c191929683
Julian Lee <julianlee@linkerviison.com>
2025-03-12 18:48:53 +0800

    More shits

--- remove_coco_anno.py
+++ remove_coco_anno.py
@@ -1,5 +1,6 @@
 import pathlib
 import json
+import tqdm
 
 coco_root ='/mnt/lighthouseACD/ACD-gdino-COCO'
 dataverse_list =[
@@ -9,21 +10,23 @@ dataverse_list =[
     'Water_Resources_20250106_image_list_keep_0.95',
 ]
 revision_list = [
-    'Mass_Rapid_Transit_20250109_image_list_keep_0.95_0.30_0.35',
+    # 'Mass_Rapid_Transit_20250109_image_list_keep_0.95_0.30_0.35', # done upload
     'Public_Works_20241230_image_list_keep_0.95',
+
     # below seems no union
-    'Sports_Development_20241223_image_list_keep_0.95',
-    'Transportation_20241230_image_list_keep_0.95',
-    'Transportation_20250109_image_list_keep_0.95',
+    # 'Sports_Development_20241223_image_list_keep_0.95', # done remove
+    # 'Transportation_20241230_image_list_keep_0.95', # done remove
+    # 'Transportation_20250109_image_list_keep_0.95', # done remove
 ]
 revision_list = [f'{coco_root}/{x}' for x in revision_list]
 revision_json_prefix = '/mnt/data-home/mobility-multimodal/data-curation'
 revision_json = [
-    'Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95_rededuplicate.json',
+    # 'Mass_Rapid_Transit/20250109/Mass_Rapid_Transit_20250109_image_list_keep_0.95_rededuplicate.json',
     'Public_Works/20241230/Public_Works_20241230_image_list_keep_0.95_rededuplicate.json',
-    'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json',
-    'Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json',
-    'Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',
+
+    # 'Sports_Development/20241223/Sports_Development_20241223_image_list_keep_0.95_rededuplicate.json', # done remove
+    # 'Transportation/20241230/Transportation_20241230_image_list_keep_0.95_rededuplicate.json', # done remove
+    # 'Transportation/20250109/Transportation_20250109_image_list_keep_0.95_rededuplicate.json',  # done remove
 ]
 revision_json = [f'{revision_json_prefix}/{x}' for x in revision_json]
 ok_list = [
@@ -31,15 +34,12 @@ ok_list = [
     'Ports_Corporation_20250124_image_list_keep_0.95',
 ]
 
-# def remove_duplicate(split_root, image_list, dry_run=True):
-splits = list(pathlib.Path(revision_list[0]).glob('split*'))
 
-json_file = revision_json[0]
-split = splits[0]
-
-def remove_coco_duplicated(split, json_file, dry_run=True):
-    image_list = list((split / 'images').glob('*'))
+def remove_coco_duplicated(split, image_list, verbose=True, dry_run=True):
+    print(split)
+    
     anno_path = (split / 'annotations' / 'labels.json')
+    image_root = (split / 'images')
     with open(anno_path) as f:
         anno_data = json.load(f)
     remove_count = 0
@@ -47,20 +47,57 @@ def remove_coco_duplicated(split, json_file, dry_run=True):
     
     new_images_annos = []
     keep_images_ids = []
-    for anno in anno_data['images']:
-        if anno['file_name'] in image_list:
+    print('checking images')
+    print('image path sample')
+    print(anno_data['images'][0]['file_name'])
+    print(image_list[0])
+    image_set = set(image_list)
+    for anno in tqdm.tqdm(anno_data['images']):
+        if anno['file_name'] in image_set:
             new_images_annos.append(anno)
             keep_images_ids.append(anno['id'])
             keep_count += 1
         else:
             remove_count += 1
+            image_path = image_root / anno['file_name']
+            if image_path.exists():
+                if verbose:
+                    print(f'removing {image_path}')
+                if not dry_run:
+                    image_path.unlink()
     
+    print('checking annotations')
     new_annotations_annos = []
-    for anno in anno_data['annotations']:
+    for anno in tqdm.tqdm(anno_data['annotations']):
         if anno['image_id'] in keep_images_ids:
             new_annotations_annos.append(anno)
     
     anno_data['images'] = new_images_annos
     anno_data['annotations'] = new_annotations_annos
 
-import ipdb; ipdb.set_trace()
+    print(f'{remove_count=}, {keep_count=}\n')
+    if not dry_run:
+        with open(anno_path, 'w') as f:
+            json.dump(anno_data, f)
+    # import ipdb; ipdb.set_trace()
+    return keep_count, remove_count
+
+
+all_keep, all_removed = 0, 0
+for revision_folder, json_file in zip(revision_list, revision_json): 
+    splits = list(pathlib.Path(revision_folder).glob('split*'))
+
+    with open(json_file) as f:
+        image_list = json.load(f)
+    # image_list = [pathlib.Path(x).name for x in image_list]
+    image_list = [
+        ('-').join(x.split('/')[3:]) for x in image_list
+    ]
+    print(image_list[0])
+
+    for split in splits:
+        keep, remove = remove_coco_duplicated(split, image_list, verbose=False, dry_run=True)
+        all_keep += keep
+        all_removed += remove
+    print(f'{all_keep=}')
+    print(f'{all_removed=}')
--- tools/auto_upload_dataverse.py
+++ tools/auto_upload_dataverse.py
@@ -35,10 +35,20 @@ def check_and_execute(data_path, command):
 
 
 if __name__ == "__main__":
+    print("Starting auto upload to dataverse")
     wait_time = 600
+    coco_root ='/mnt/lighthouseACD/ACD-gdino-COCO'
+    file_root_list = [
+        'Sports_Development_20241223_image_list_keep_0.95', # done remove
+        'Transportation_20241230_image_list_keep_0.95', # done remove
+        'Transportation_20250109_image_list_keep_0.95', # done remove
+    ]
+    file_root_list = [f'{coco_root}/{x}' for x in file_root_list]
+    file_path_list = []
+    for file_root in file_root_list:
+        file_path_list.extend([p for p in pathlib.Path(file_root).glob('*split*') if p.is_dir()])
+    file_path_list = [x for x in file_path_list if not (x / 'uploaded').exists()]
     while True:
-        file_root = pathlib.Path(f'/mnt/lighthouseACD/ACD-gdino-COCO/')
-        file_path_list = [p for p in file_root.rglob('*split*') if p.is_dir()]
         for file_path in file_path_list:
             print(str(file_path))
             command = f'conda run -n lighthouse python tools/import_dataset_from_local.py -host https://visionai.linkervision.ai/dataverse/curation -e julianlee@linkervision.com -p {PASSWORD} -s 2bd928e5-a98f-4aae-a093-8545c57c103f  -project 225 --folder {file_path} -name {file_path.parent.name}/{file_path.name} -type annotated_data -anno coco'
