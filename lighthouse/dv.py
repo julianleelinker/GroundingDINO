43c646a5a09ac1628e6a573be5e483f3c7c9b1f5
Julian Lee <julianlee@linkerviison.com>
2025-03-20 17:43:21 +0800

    Dataverse list project id and data slice id example

--- lighthouse/dv.py
+++ lighthouse/dv.py
@@ -1,16 +1,20 @@
 from dataverse_sdk import *
 from dataverse_sdk.connections import get_connection
+from common import DATAVERSE_PASSWORD, DATAVERSE_CKPT1_PROJECT_ID, DATAVERSE_CKPT2_PROJECT_ID
+
+
 client = DataverseClient(
-    host=DataverseHost.PRODUCTION.value, email="julianlee@linkervision.com ", password="Baoyun5820", service_id="2bd928e5-a98f-4aae-a093-8545c57c103f", alias="default", force = True
-)
+            host=DataverseHost.PRODUCTION.value ,
+            email="julianlee@linkervision.com", 
+            password=DATAVERSE_PASSWORD,
+            service_id="2bd928e5-a98f-4aae-a093-8545c57c103f",
+            # alias="prod",
+            alias="default",
+        )
 assert client is get_connection("default")
- 
-# user = client.get_user()
- 
-client.list_projects()
- 
 projects = client.list_projects(current_user = False,)
                                 # exclude_sensor_type=SensorType.LIDAR,
                                 # image_type= OntologyImageType._2D_BOUNDING_BOX)
- 
+ckpt1_data_slice = client.list_dataslices(project_id=DATAVERSE_CKPT1_PROJECT_ID, client_alias=client.alias)
+ckpt2_data_slice = client.list_dataslices(project_id=DATAVERSE_CKPT2_PROJECT_ID, client_alias=client.alias)
 import ipdb; ipdb.set_trace()
