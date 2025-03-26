cc2e54c9ecde8e3f38120473c70c3cb75fcab851
Julian Lee <julianlee@linkerviison.com>
2025-03-26 16:26:09 +0800

    Add some query examples to inspect_data_stats

--- lighthouse/inspect_data_stats.py
+++ lighthouse/inspect_data_stats.py
@@ -1,22 +1,28 @@
 from common import VLM_CKPT1_FOLDERS, VLM_CKPT2_FOLDERS, DINO_COCO_ROOT, VLM_ANNOTATION_ROOT
-from common import load_stats_fwf
 import pandas as pd
 
 
-numeric_cols = ["number", "annotated_number", "ckpt_data_number"]
+bbox = pd.read_csv(f"{DINO_COCO_ROOT}/dino_coco_stats.csv")
+bbox_depr = bbox[bbox["notes"] == "deprecated"]
+bbox_uploaded = bbox[(bbox["uploaded"] == True) & (bbox["notes"] == "deprecated")]
+bbox_depr_done = bbox_depr[bbox_depr["annotated"]!=0]
+bbox_depr_done_uploaded = bbox_depr_done[bbox_depr_done["uploaded"]]
+bbox_new = bbox[bbox["notes"] == "no"]
 
-df_bbox = load_stats_fwf(f"{DINO_COCO_ROOT}/dino_coco_data_stats.txt", numeric_cols)
-df_deprecated = df_bbox[df_bbox["notes"] == "deprecated"]
-df_bbox_uploaded = df_bbox[(df_bbox["is_uploaded"] == True) & (df_bbox["notes"] == "deprecated")]
-df_deprecated_done = df_deprecated[df_deprecated["annotated_number"]!=0]
-df_deprecated_done_uploaded = df_deprecated_done[df_deprecated_done["is_uploaded"]]
-df_new = df_bbox[df_bbox["notes"] == "no"]
+bbox_depr_done.groupby("depart")["annotated"].sum()
+bbox_new.groupby("depart")["number"].sum()
 
-df_deprecated_done.groupby("depart")["annotated_number"].sum()
-df_new.groupby("depart")["number"].sum()
+print("current done bbox data stats")
+print(bbox_depr_done.groupby("depart")["annotated"].sum().apply(lambda x: f"{x:,}"))
+print("current new bbox data stats")
+print(bbox_new.groupby("depart")["number"].sum().apply(lambda x: f"{x:,}"))
+pc_new = bbox_new[bbox_new["depart"]=="Ports_Corporation"]
+sd_new = bbox_new[bbox_new["depart"]=="Sports_Development"]
+mrt_new = bbox_new[bbox_new["depart"]=="Mass_Rapid_Transit"]
 
-vlm_loaded = load_stats_fwf(f"{VLM_ANNOTATION_ROOT}/vlm_data_stats.txt", numeric_cols)
-ckpt1_2 = vlm_loaded[vlm_loaded["ckpt_status"].isin(["ckpt1", "ckpt2"])]
+
+vlm_loaded = pd.read_csv(f"{VLM_ANNOTATION_ROOT}/vlm_data_stats.csv")
+ckpt1_2 = vlm_loaded[vlm_loaded["ckpt"].isin(["ckpt1", "ckpt2"])]
 
 import ipdb; ipdb.set_trace()
 
