3f7a67099887adcade992f4b4adab0e74c6dbb84
Julian Lee <julianlee@linkerviison.com>
2025-04-12 13:48:24 +0800

    Update copy new acd

--- lighthouse/copy_new_acd.py
+++ lighthouse/copy_new_acd.py
@@ -6,15 +6,13 @@ from common import AUGMENTED_CURATED_RUNNING_JSONS, DINO_COCO_SOURCE_ROOT, copy_
 
 def main():
     json_list = AUGMENTED_CURATED_RUNNING_JSONS
-    json_list = [x for x in json_list if "China_Steel" in str(x)]
 
     dst_root = pathlib.Path(DINO_COCO_SOURCE_ROOT)
-    dst_root.mkdir(exist_ok=True, parents=True)
-    os.chmod(dst_root, 0o777)
 
     print("Copying images from json files to target folder...")
     for json_path in tqdm.tqdm(json_list):
         dst = dst_root / json_path.stem
+        print(dst)
         dst.mkdir(exist_ok=True, parents=True)
         os.chmod(dst, 0o777)
         copy_images_in_json(json_path, dst, is_image_list=True, split_size=10000)
