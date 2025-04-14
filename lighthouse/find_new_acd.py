c723c5264b1253ad543215258434e63594f158e9
Julian Lee <julianlee@linkerviison.com>
2025-04-14 18:32:06 +0800

    Add script to find new augmented-curated-data

--- lighthouse/find_new_acd.py
+++ lighthouse/find_new_acd.py
@@ -2,13 +2,15 @@ import pathlib
 from common import get_depart, DATA_CURATION_ROOT, AUGMENTED_CURATED_EXCLUDED_JSONS
 
 
-json_list = [path for path in pathlib.Path(DATA_CURATION_ROOT).rglob('*image_list_keep*') if path.is_file()]
-print(len(json_list))
-json_list = [x for x in json_list if x not in AUGMENTED_CURATED_EXCLUDED_JSONS]
-print(len(json_list))
+def main():
+    json_list = [path for path in pathlib.Path(DATA_CURATION_ROOT).rglob('*image_list_keep*') if path.is_file()]
+    print(len(json_list))
+    json_list = [x for x in json_list if x not in AUGMENTED_CURATED_EXCLUDED_JSONS]
+    print(len(json_list))
 
-import ipdb; ipdb.set_trace()
-for json_path in json_list:
-    print(json_path)
+    for json_path in json_list:
+        print(f'"{json_path}",')
 
 
+if __name__ == "__main__":
+    main()
