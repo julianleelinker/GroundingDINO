054c75554413041d74cf5a8c2e9a894afb27707f
Julian Lee <julianlee@linkerviison.com>
2025-05-06 17:47:06 +0800

    Change vlm_count_stats.py

--- lighthouse/vlm_count_stats.py
+++ lighthouse/vlm_count_stats.py
@@ -30,40 +30,46 @@ def get_depart_image_numbers_vlm(folder: str | pathlib.Path) -> dict[str, int]:
 
 
 if __name__ == "__main__":
-    new_data_col_name = "NEW 0407"
-    column_names = ["gov checked", "uploaded not QA", new_data_col_name, "TOTAL"]
+    new_data_col_name = "NEW"
+    # new_folders = VLM_ADDDED_0407_FOLDERS
+    new_folders = []
+    column_names = ["uploaded QA", "uploaded not QA", "QA done", "gov checked",  new_data_col_name, "TOTAL"]
     row_names = DEPARTS_EN + ["total"]
     df = pd.DataFrame(0, index=row_names, columns=column_names)
-    vlm_root = "/mnt/data-home/mobility-multimodal/checkpoint/vlm/gov0415"
-    vlm_2_handed_fodlers = [
-        "/mnt/data-home/mobility-multimodal/checkpoint/vlm/hand/Water_Resources/20250213/Water_Resources_20250213_curated_t1",
-        "/mnt/data-home/mobility-multimodal/checkpoint/vlm/hand/Transportation/20250115/Transportation_20250115_curated_t4"
-    ]
-    vlm_2_handed_fodlers = [pathlib.Path(x) for x in vlm_2_handed_fodlers]
-    vlm_2_to_1_names = [x.name for x in vlm_2_handed_fodlers]
-    qa_and_gov_folders = find_depth3_subfolders(vlm_root)
-    print(f"{len(qa_and_gov_folders)=}")
-    uploaded_not_qa_folders = VLM_CKPT2_FOLDERS
-    uploaded_not_qa_folders = [x for x in uploaded_not_qa_folders if x.name not in vlm_2_to_1_names]
-    import ipdb; ipdb.set_trace()
+
+    gov_checked_folders = find_depth3_subfolders("/mnt/data-home/mobility-multimodal/checkpoint/vlm/gov0415")
+    qa_folders = list(pathlib.Path("/mnt/data-home/mobility-multimodal/checkpoint/vlm/hand0505").glob("*"))
+    qa_folders_set = {x.name for x in qa_folders}
+    print(f"{len(qa_folders)=}")
+
+    uploaded_folders = VLM_CKPT2_FOLDERS
+    uploaded_folders.extend(VLM_ADDDED_0407_FOLDERS)
+    print(f"{len(uploaded_folders)=}")
+    uploaded_not_qa_folders = [x for x in uploaded_folders if x.name not in qa_folders_set]
+    print(f"{len(uploaded_not_qa_folders)=}")
+    uploaded_qa_folders = [x for x in uploaded_folders if x.name in qa_folders_set]
 
     # for folder in VLM_ADDDED_0407_FOLDERS[:-1]:
     name_to_list = {
-        new_data_col_name: VLM_ADDDED_0407_FOLDERS,
-        "gov checked": qa_and_gov_folders,
+        new_data_col_name: new_folders,
+        "QA done": qa_folders,
+        "gov checked": gov_checked_folders,
         "uploaded not QA": uploaded_not_qa_folders,
+        "uploaded QA": uploaded_qa_folders,
     }
     for name, folder_list in tqdm.tqdm(name_to_list.items()):
         for folder in tqdm.tqdm(folder_list):
             depart, image_number = get_depart_image_numbers_vlm(folder)
             df.loc[depart, name] += image_number
 
-    df["TOTAL"] = df["gov checked"] + df["uploaded not QA"] + df[new_data_col_name]
+    df["TOTAL"] = df["gov checked"] + df["uploaded not QA"] + df["QA done"] + df[new_data_col_name]
 
     df.loc["total"] = df.sum(axis=0)
 
     df_formatted = df.map(lambda x: f"{x:,}")
     df_formatted = df_formatted.rename_axis('depart', axis='columns')
     print(df_formatted)
+    df_showed = df_formatted[["uploaded not QA", "QA done", "gov checked", new_data_col_name, "TOTAL"]]
+    print(df_showed)
 
     import ipdb; ipdb.set_trace()
