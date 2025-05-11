a7c964645e4738916c961ad4264863d52fb03fcd
Julian Lee <julianlee@linkerviison.com>
2025-05-11 15:33:26 +0800

    Copy public work first 22 splits

--- lighthouse/bbox_copy_images_in_json.py
+++ lighthouse/bbox_copy_images_in_json.py
@@ -1,11 +1,11 @@
 import os
 import pathlib
 import tqdm
-from common import DINO_COCO_SOURCE_ROOT, copy_images_in_json, AUGMENTED_CURATED_JSONS_0508
+from common import DINO_COCO_SOURCE_ROOT, copy_images_in_json, AUGMENTED_CURATED_JSONS_0509
 
 
 def main():
-    json_list = AUGMENTED_CURATED_JSONS_0508
+    json_list = AUGMENTED_CURATED_JSONS_0509
 
     dst_root = pathlib.Path(DINO_COCO_SOURCE_ROOT)
 
