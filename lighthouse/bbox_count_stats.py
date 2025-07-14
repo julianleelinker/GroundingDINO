98b56582f45adcad4b20ac8a9451849121906e19
Julian Lee <julianlee@linkerviison.com>
2025-07-14 16:10:40 +0800

    Assuming July uploaded bbox_count

--- lighthouse/bbox_count_stats.py
+++ lighthouse/bbox_count_stats.py
@@ -20,7 +20,16 @@ new_json_list = [x for x in new_json_list if x not in AUGMENTED_CURATED_EXCLUDED
 new_json_list = [x for x in new_json_list if get_depart(x) is not None] # this line exclude illegal json
 new_json_list = [x for x in new_json_list if get_depart(x) != "Bus"] # this line exclude bus
 new_json_list = [x for x in new_json_list if "deprecated" not in str(x)] # this line exclude the deprecated json
-new_json_list = AUGMENTED_CURATED_JSONS_0703
+
+
+for json_path in AUGMENTED_CURATED_EXCLUDED_JSONS:
+    if not "Public_Works" in str(json_path):
+        continue
+    with open(json_path, 'r') as f:
+        image_list = json.load(f)
+    print(json_path)
+    print(f"{len(image_list)=}\n")
+
 
 uploaded_root_to_pattern = {
     "/mnt/data-home/mobility-multimodal/revised_bbox/deduplicated": "*/*",
@@ -61,6 +70,16 @@ print(f"{len(prev_uploaded_folders)=}")
 print(f"{len(not_qa_folder_list)=}")
 print(f"{len(done_qa_folder_list)=}")
 
+import ipdb; ipdb.set_trace()
+public_works_folder_list = [x for x in not_qa_folder_list if get_depart(x)=="Public_Works"]
+public_works_folder_list = sorted(public_works_folder_list)
+public_works_folder_list = public_works_folder_list[:64] 
+accum_count = [0]
+for x in public_works_folder_list:
+    image_list = list((x/"images").glob("*")) 
+    accum_count.append(accum_count[-1] + len(image_list))
+    print(accum_count[-1])
+import ipdb; ipdb.set_trace()
 
 # for x in qa_passed_folder_list:
 #     if not any(get_split_name(x) in str(s) for s in prev_uploaded_folders):
@@ -120,4 +139,36 @@ date_str = today.strftime("%m/%d")
 print(f"\n# bbox updated {date_str}")
 print(df_showed)
 
+import ipdb; ipdb.set_trace()
+
+
+#temporary_added for counting assuming July is uploaded
+count_dict = {
+    'Public_Works':       299_691,
+    'Sports_Development':  18_940,
+    'Mass_Rapid_Transit': 133_357,
+    'Ports_Corporation':   40_000,
+    'Transportation':     267_186,
+}
+for key, val in count_dict.items():
+    if key == 'Transportation':
+        df.loc[key, "infer this month"] -= val
+    else:
+        df.loc[key, "not QA/merged"] -= val
+    df.loc[key, "pass QA/merged"] += val
+print(df)
+
+df["total"] = df["not QA/merged"] + df["pass QA/merged"] + df["infer this month"]
+df.loc["total"]=0
+df.loc["total"] = df.sum(axis=0)
+
+df_formatted = df.map(lambda x: f"{x:,}")
+df_formatted = df_formatted.rename_axis('depart', axis='columns')
+print(df_formatted)
+df_showed = df_formatted[["not QA/merged", "done QA/merged", "pass QA/merged", "new json", "infer this month", "total"]]
+
+today = date.today()
+date_str = today.strftime("%m/%d")
+print(f"\n# bbox updated {date_str}")
+print(df_showed)
 import ipdb; ipdb.set_trace()
